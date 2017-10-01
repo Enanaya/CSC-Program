55b92cbcd93ed1fc136bc720101aa00c5a5752ae
76500 <76500@windows10.microdone.cn>
2017-10-01 15:09:47 +0100

    输出字符窜

--- HW/src/hello_world.java
+++ HW/src/hello_world.java
@@ -1,7 +1,9 @@
 
 public class hello_world {
 	public static void main(String[] args){
-	System.out.println("Hello Woirld");
+		String hw;
+		hw="Hello World";
+	System.out.println(hw);
 	/**
 	 doc
 	 */
