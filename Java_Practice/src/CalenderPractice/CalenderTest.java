809f18bbcd5de630cc95c42b943247f13e79a81c
76500 <76500@windows10.microdone.cn>
2017-10-08 18:50:37 +0100

    模拟日历v1.1----小补充说明

--- Java_Practice/src/CalenderPractice/CalenderTest.java
+++ Java_Practice/src/CalenderPractice/CalenderTest.java
@@ -17,17 +17,16 @@ public class CalenderTest {
 		try {
 			Calendar c = new GregorianCalendar();
 			c.setTime(format.parse("2030-2-10"));// 将字符串解析成date
-			c.set(Calendar.DATE, 1); // 设定日期
-			// System.out.print("请输入月份 ");
-			// int month=System.in.read();
-			// c.set(Calendar.MONTH, month-1);
+			c.set(Calendar.DATE, 1); // 设定日期为1号，方便获取第一天是星期几
+			
+			System.out.print("请输入月份 ");  //可以自己选择月份
+			int month = System.in.read();
+			c.set(Calendar.MONTH, month - 1);
 			int Max_day = c.getActualMaximum(Calendar.DATE);
 
 			// System.out.println(c.get(Calendar.DAY_OF_WEEK));
-			System.out.println("月份\t" + (c.get(Calendar.MONTH) + 1));
-			System.out.println("总天数 " + c.getActualMaximum(Calendar.DATE));
-			// System.out.println(c.getTime());
-			// System.out.println(c.get(Calendar.DAY_OF_WEEK));
+			System.out.println("月份 \t" + (c.get(Calendar.MONTH) + 1));
+			System.out.println("总天数 \t" + c.getActualMaximum(Calendar.DATE));
 			System.out.println("日\t一\t二\t三\t四\t五\t六");
 			// 获取月份第一天是星期几，其中方法获取星期天时返回1，其余减一即为正确
 			int dayforweek = c.get(Calendar.DAY_OF_WEEK);
@@ -37,7 +36,7 @@ public class CalenderTest {
 				dayforweek--;
 			}
 
-			int j = dayforweek;
+			int j = dayforweek; // 可简化，可以给别人练习
 			for (int i = 1; i <= Max_day; i++) {
 				if (i <= dayforweek) {
 					System.out.print(" \t");
