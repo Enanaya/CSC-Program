e52ef34f2e74c7846c80cf82e40a51179df35433
Enanaya <765009135@qq.com>
2018-04-24 23:12:08 +0100

    初始化

---
+++ advancedCW2/src/classpackage/BinarySemaphore.java
@@ -0,0 +1,13 @@
+package classpackage;
+
+public class BinarySemaphore extends Semaphore{
+	public BinarySemaphore() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public BinarySemaphore(int initial) {
+		// TODO Auto-generated constructor stub
+		super(initial);
+	}
+}
---
+++ advancedCW2/src/classpackage/Test.java
@@ -0,0 +1,10 @@
+package classpackage;
+
+import java.util.Random;
+
+public class Test {
+	public static void main(String[] args) {
+		double step = Math.random();
+		
+	}
+}
